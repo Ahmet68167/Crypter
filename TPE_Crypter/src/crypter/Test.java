34fdc47d7a29d9e20b0f16471b6ae4bf63fc50f8
Ahmet <Ahmet@Ahmet-PC.localdomain>
2014-12-03 19:27:55 +0100

    InterableCrypter noch fehlerhaft

--- TPE_Crypter/src/crypter/Test.java
+++ TPE_Crypter/src/crypter/Test.java
@@ -1,7 +1,10 @@
 package crypter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+import crypter.CrypterFactory.IterableCrypter;
 import crypter.exception.CrypterException;
 import crypter.exception.IllegalKeyException;
 
@@ -10,12 +13,16 @@ public class Test {
 	public static void main(String[] args) {
 	
 		CrypterFactory a = new CrypterFactory();
+		List<String> s = Arrays.asList("HALLO", "KLAUS", "AHMET");
+		
 		
 		try {
-			CrypterFactory.Crypter sub = a.createCrypter("MNBVCXYLKJHGFDSAPOIUZTREWQ", CrypterFactory.Crypt.SUBSTITUTION);
-			CrypterFactory.Crypter cae = a.createCrypter("L", CrypterFactory.Crypt.CAESAR);
-			CrypterFactory.Crypter um = a.createCrypter("", CrypterFactory.Crypt.REVERSE);
-			CrypterFactory.Crypter xor = a.createCrypter("IAMTHEONEWHOKNOCKS", CrypterFactory.Crypt.XOR);
+			Crypter sub = a.createCrypter("MNBVCXYLKJHGFDSAPOIUZTREWQ", CrypterFactory.Crypt.SUBSTITUTION);
+			Crypter cae = a.createCrypter("L", CrypterFactory.Crypt.CAESAR);
+			Crypter um = a.createCrypter("", CrypterFactory.Crypt.REVERSE);
+			Crypter xor = a.createCrypter("IAMTHEONEWHOKNOCKS", CrypterFactory.Crypt.XOR);
+			
+			IterableCrypter c = new IterableCrypter(new IterableCrypter(s, um), cae);
 			
 			try {
 				System.out.println(sub.decrypt(cae.decrypt(um.decrypt(xor.decrypt("QOZEG]A[UXDKZIZLAB\\NUQIO^^RXYHADV[EFFJ\\\\[\\U_]YDVZABDZT\\V\\SKB@X")))));
@@ -28,6 +35,7 @@ public class Test {
 			
 			e.printStackTrace();
 		}
+		
 		
 		// QOZEG]A[UXDKZIZLAB\NUQIO^^RXYHADV[EFFJ\\[\U_]YDVZABDZT\V\SKB@X
 
