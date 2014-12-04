dfb9667966c0052cf0752031660e18244f0023fa
Ahmet <Ahmet@Ahmet-PC.localdomain>
2014-12-04 14:17:00 +0100

    Erstellt und fertig

---
+++ TPE_Crypter/src/crypter/iterable/IterableDecrypter.java
@@ -0,0 +1,48 @@
+package crypter.iterable;
+
+import java.util.Iterator;
+
+import crypter.Crypter;
+import crypter.exception.CrypterException;
+
+public class IterableDecrypter implements Iterable<String>{
+
+	private Iterator<String> iterator;
+	private Crypter crypter;
+	
+	private IterableDecrypter(Crypter crypter) {
+		this.crypter = crypter;
+	}
+	
+	public IterableDecrypter(Iterable list, Crypter crypter) {
+		this(crypter);
+		this.iterator = list.iterator();
+	}
+	
+	@Override
+	public Iterator<String> iterator() {
+		
+		return new Iterator<String>() {
+			
+			@Override
+			public boolean hasNext() {
+				return iterator.hasNext();
+			}
+
+			@Override
+			public String next() {
+				String txt = "";
+				
+				try {
+					txt = crypter.decrypt(iterator.next());
+				} catch (CrypterException e) {
+					e.printStackTrace();
+				}
+				
+				return txt;
+			}
+			
+		};
+	}
+	
+}
