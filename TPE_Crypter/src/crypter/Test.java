39c2a96160b3faf6eeef4088e2495b29a8c853d8
Ahmet <Ahmet@Ahmet-PC.localdomain>
2014-12-04 14:17:06 +0100

    Test

--- TPE_Crypter/src/crypter/Test.java
+++ TPE_Crypter/src/crypter/Test.java
@@ -7,36 +7,26 @@ import java.util.List;
 import crypter.exception.CrypterException;
 import crypter.exception.IllegalKeyException;
 import crypter.iterable.IterableCrypter;
+import crypter.iterable.IterableDecrypter;
 
 public class Test {
 
 	public static void main(String[] args) {
 	
-		List<String> s = Arrays.asList("HALLO", "KLAUS", "AHMET");
-		
+		List<String> s = Arrays.asList("QOZEG]A[UXDKZIZLAB\\NUQIO^^RXYHADV[EFFJ\\\\[\\U_]YDVZABDZT\\V\\SKB@X");
+		List<String> t = Arrays.asList("PMMBI", "TVBML", "UFNIB");
 		
 		try {
 			Crypter sub = CrypterFactory.createCrypter("MNBVCXYLKJHGFDSAPOIUZTREWQ", CrypterFactory.Crypt.SUBSTITUTION);
-			Crypter cae = CrypterFactory.createCrypter("A", CrypterFactory.Crypt.CAESAR);
+			Crypter cae = CrypterFactory.createCrypter("L", CrypterFactory.Crypt.CAESAR);
 			Crypter um = CrypterFactory.createCrypter("", CrypterFactory.Crypt.REVERSE);
 			Crypter xor = CrypterFactory.createCrypter("IAMTHEONEWHOKNOCKS", CrypterFactory.Crypt.XOR);
 			
-			IterableCrypter c = new IterableCrypter(s, cae);
-			IterableCrypter d = new IterableCrypter(new IterableCrypter(s, cae), um);
+			IterableDecrypter c = new IterableDecrypter(new IterableDecrypter(new IterableDecrypter(new IterableDecrypter(s, xor), um), cae), sub);
 			
 			for(String msg : c)
 				System.out.println(msg);
-			
-			for(String msg : d)
-				System.out.println(msg);
-			
-			try {
-				System.out.println(sub.decrypt(cae.decrypt(um.decrypt(xor.decrypt("QOZEG]A[UXDKZIZLAB\\NUQIO^^RXYHADV[EFFJ\\\\[\\U_]YDVZABDZT\\V\\SKB@X")))));
-
-			} catch (CrypterException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	
 		} catch (IllegalKeyException e) {
 			
 			e.printStackTrace();
