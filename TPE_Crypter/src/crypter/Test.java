181f028cc4431816f6e05bae383191e50a7daf7a
1410650 <1410650@141.19.7.4>
2014-12-04 13:16:12 +0100

    IterableCrypter a = new IterableCrypter(new IterableCryper(list, caesar) , reverse) funktioniert noch nicht

--- TPE_Crypter/src/crypter/Test.java
+++ TPE_Crypter/src/crypter/Test.java
@@ -4,25 +4,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import crypter.CrypterFactory.IterableCrypter;
 import crypter.exception.CrypterException;
 import crypter.exception.IllegalKeyException;
+import crypter.iterable.IterableCrypter;
 
 public class Test {
 
 	public static void main(String[] args) {
 	
-		CrypterFactory a = new CrypterFactory();
 		List<String> s = Arrays.asList("HALLO", "KLAUS", "AHMET");
 		
 		
 		try {
-			Crypter sub = a.createCrypter("MNBVCXYLKJHGFDSAPOIUZTREWQ", CrypterFactory.Crypt.SUBSTITUTION);
-			Crypter cae = a.createCrypter("L", CrypterFactory.Crypt.CAESAR);
-			Crypter um = a.createCrypter("", CrypterFactory.Crypt.REVERSE);
-			Crypter xor = a.createCrypter("IAMTHEONEWHOKNOCKS", CrypterFactory.Crypt.XOR);
+			Crypter sub = CrypterFactory.createCrypter("MNBVCXYLKJHGFDSAPOIUZTREWQ", CrypterFactory.Crypt.SUBSTITUTION);
+			Crypter cae = CrypterFactory.createCrypter("A", CrypterFactory.Crypt.CAESAR);
+			Crypter um = CrypterFactory.createCrypter("", CrypterFactory.Crypt.REVERSE);
+			Crypter xor = CrypterFactory.createCrypter("IAMTHEONEWHOKNOCKS", CrypterFactory.Crypt.XOR);
 			
-			IterableCrypter c = new IterableCrypter(new IterableCrypter(s, um), cae);
+			IterableCrypter c = new IterableCrypter(s, cae);
+			IterableCrypter d = new IterableCrypter(new IterableCrypter(s, cae), um);
+			
+			for(String msg : c)
+				System.out.println(msg);
+			
+			for(String msg : d)
+				System.out.println(msg);
 			
 			try {
 				System.out.println(sub.decrypt(cae.decrypt(um.decrypt(xor.decrypt("QOZEG]A[UXDKZIZLAB\\NUQIO^^RXYHADV[EFFJ\\\\[\\U_]YDVZABDZT\\V\\SKB@X")))));
