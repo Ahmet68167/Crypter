22a7df94104fcd3ef10d48faec2d00e5540f5bd4
ahmet <ahmet@10.10.101.131>
2014-12-16 11:34:12 +0100

    Java Doc hinzugefuegt

--- TPE_Crypter/src/crypter/exception/IllegalKeyException.java
+++ TPE_Crypter/src/crypter/exception/IllegalKeyException.java
@@ -1,11 +1,29 @@
 package crypter.exception;
 
+
+/**
+ * Gibt eine IllegalKeyException aus, wenn der eingegebene
+ * Schluessel ungueltig ist.
+ *
+ */
 public class IllegalKeyException extends CrypterException {
 
+	
+	/**
+	 * Gbt den folgenden String aus:
+	 * "Der eingegebene Schluessel ist ungueltig"
+	 * 
+	 */
 	public IllegalKeyException() {
 		super("Der eingegebene Schluessel ist ungueltig.");
 	}
 	
+	
+	/**
+	 * Gibt den uebergebenen String aus
+	 * 
+	 * @param s - String, der ausgegeben werden soll
+	 */
 	public IllegalKeyException(String s) {
 		super(s);
 	}
