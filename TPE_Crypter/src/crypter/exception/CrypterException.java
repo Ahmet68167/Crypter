238bfa4c8fa0f5753ef95b0e58219beef6aa252c
ahmet <ahmet@10.10.101.131>
2014-12-16 11:36:44 +0100

    Java Doc hinzugefuegt

--- TPE_Crypter/src/crypter/exception/CrypterException.java
+++ TPE_Crypter/src/crypter/exception/CrypterException.java
@@ -1,11 +1,27 @@
 package crypter.exception;
 
+/**
+ * Wirft eine Exception, wenn ein Problem bei der
+ * Verschluesselung auftritt
+ *
+ */
 public class CrypterException extends Exception {
 
+	/**
+	 * Gibt den folgenden String aus:
+	 * "Es ist ein Fehler mit der Verschluesselung aufgetreten"
+	 * 
+	 */
 	public CrypterException() {
 		super("Es ist ein Fehler mit der Verschluesselung aufgetreten");
 	}
 	
+	
+	/**
+	 * Gibt den uebergebenen String aus
+	 * 
+	 * @param s - String, der ausgegeben werden soll
+	 */
 	public CrypterException(String s) {
 		super(s);
 	}
